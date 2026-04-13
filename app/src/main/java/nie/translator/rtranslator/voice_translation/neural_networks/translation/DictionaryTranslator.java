812ef6fb6fa19525a95b54a517f420c65f907253
niedev <contact.niedev@gmail.com>
2026-04-13 23:31:42 +0200

    Initial implementation of native dictionary translation system

---
+++ app/src/main/java/nie/translator/rtranslator/voice_translation/neural_networks/translation/DictionaryTranslator.java
@@ -0,0 +1,17 @@
+package nie.translator.rtranslator.voice_translation.neural_networks.translation;
+
+import nie.translator.rtranslator.tools.CustomLocale;
+
+public class DictionaryTranslator {
+    public static void loadDictionary(CustomLocale lang){
+
+    }
+
+    public static void unloadDictionary(CustomLocale lang){
+
+    }
+
+    public static void translateWord(String word, CustomLocale srcLang, CustomLocale trgLang){
+
+    }
+}
